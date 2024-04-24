0c391e78728ffdef147d443866002809eb37e2d5
Bruno Tito <brunosantostito2@hotmail.com>
2024-04-23 22:00:21 -0300

    adição do parametro cancelationToken nos metodos assincronos que manipulam o bd

--- Estudantes/EstudantesRotas.cs
+++ Estudantes/EstudantesRotas.cs
@@ -9,57 +9,57 @@ namespace ApiCrud.Estudantes
         {
             var rotasEstudantes = app.MapGroup("estudantes");
 
-            rotasEstudantes.MapPost("", async (AddEstudanteRequest request, AppDbContext context) =>
+            rotasEstudantes.MapPost("", async (AddEstudanteRequest request, AppDbContext context, CancellationToken ct) =>
             {
-                var jaExiste = await context.Estudantes.AnyAsync(e => e.Nome == request.Nome);
+                var jaExiste = await context.Estudantes.AnyAsync(e => e.Nome == request.Nome, ct);
 
                 if (jaExiste) return Results.Conflict("Estudante já cadastrado");
 
                 var novoEstudante = new Estudante(request.Nome);
 
-                await context.Estudantes.AddAsync(novoEstudante);
-                await context.SaveChangesAsync();
+                await context.Estudantes.AddAsync(novoEstudante, ct);
+                await context.SaveChangesAsync(ct);
 
                 var estudanteRetorno = new EstudanteDto(novoEstudante.Id, novoEstudante.Nome);
                 return Results.Ok(estudanteRetorno);
             });
 
 
-            rotasEstudantes.MapGet("", async (AppDbContext context) =>
+            rotasEstudantes.MapGet("", async (AppDbContext context, CancellationToken ct) =>
             {
                 var estudantes = await context.Estudantes
                 .Where(e => e.Ativo)
                 .Select(e => new EstudanteDto(e.Id, e.Nome))
-                .ToListAsync();
+                .ToListAsync(ct);
 
 
                 return estudantes;
             });
 
-            rotasEstudantes.MapPut("{id}", async (Guid id, UpdateEstudanteRequest request, AppDbContext context) =>
+            rotasEstudantes.MapPut("{id}", async (Guid id, UpdateEstudanteRequest request, AppDbContext context, CancellationToken ct) =>
             {
-                var estudante = await context.Estudantes.SingleOrDefaultAsync(e => e.Id == id);
+                var estudante = await context.Estudantes.SingleOrDefaultAsync(e => e.Id == id, ct);
 
                 if (estudante == null) return Results.NotFound();
 
-                var jaExiste = await context.Estudantes.AnyAsync(e => e.Nome == request.Nome);
+                var jaExiste = await context.Estudantes.AnyAsync(e => e.Nome == request.Nome, ct);
                 if (jaExiste) return Results.Conflict("Estudante já cadastrado");
 
                 estudante.AtualizarNome(request.Nome);
 
-                await context.SaveChangesAsync();
+                await context.SaveChangesAsync(ct);
                 return Results.Ok(new EstudanteDto(estudante.Id, estudante.Nome));
             });
 
-            rotasEstudantes.MapDelete("{id}", async (Guid id, AppDbContext context) =>
+            rotasEstudantes.MapDelete("{id}", async (Guid id, AppDbContext context, CancellationToken ct) =>
             {
-                var estudante = await context.Estudantes.SingleOrDefaultAsync(e => e.Id == id);
+                var estudante = await context.Estudantes.SingleOrDefaultAsync(e => e.Id == id, ct);
 
                 if (estudante == null) return Results.NotFound();
 
                 estudante.DesativarEstudante();
 
-                await context.SaveChangesAsync();
+                await context.SaveChangesAsync(ct);
                 return Results.Ok();
             });
 
